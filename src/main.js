05a98a811b0a5287434755ea215521b67f410315
FaridMo99 <farid-ahmad@hotmail.de>
2025-03-11 16:41:47 +0100

    added sticky header and fixed formatting

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import { format } from 'date-fns';
+import "./style.css";
+import { format } from "date-fns";
 
 const currentDateAndTime = {
-  dateInput: document.querySelector('#date'),
-  timeInput: document.querySelector('#time'),
+  dateInput: document.querySelector("#date"),
+  timeInput: document.querySelector("#time"),
 
   run() {
     this.setMinDateAndTime();
@@ -13,76 +13,82 @@ const currentDateAndTime = {
   setMinDateAndTime() {
     const now = new Date();
 
-    const currentDate = format(now, 'yyyy-MM-dd');
-    const currentTime = format(now, 'HH:mm');
+    const currentDate = format(now, "yyyy-MM-dd");
+    const currentTime = format(now, "HH:mm");
 
-    this.dateInput.setAttribute('min', currentDate);
-    this.timeInput.setAttribute('min', currentTime);
+    this.dateInput.setAttribute("min", currentDate);
+    this.timeInput.setAttribute("min", currentTime);
   },
 
   addDateChangeListener() {
-    this.dateInput.addEventListener('change', () => {
+    this.dateInput.addEventListener("change", () => {
       const selectedDate = new Date(this.dateInput.value);
       const currentDateObj = new Date();
 
       if (selectedDate.toDateString() === currentDateObj.toDateString()) {
-        this.timeInput.setAttribute('min', format(currentDateObj, 'HH:mm'));
+        this.timeInput.setAttribute("min", format(currentDateObj, "HH:mm"));
       } else {
-        this.timeInput.removeAttribute('min');
+        this.timeInput.removeAttribute("min");
       }
     });
-  }
+  },
 };
 
 const modal = {
-    openModalButton: document.querySelector(".add"),
-    modalDialog: document.querySelector("dialog"),
-    closeModalButton: document.querySelector(".modalClose"),
-    modalDate: document.querySelector("#date"),
-    modalTime: document.querySelector("#time"),
-    modalTask: document.querySelector("#task"),
+  openModalButton: document.querySelector(".add"),
+  modalDialog: document.querySelector("dialog"),
+  closeModalButton: document.querySelector(".modalClose"),
+  modalDate: document.querySelector("#date"),
+  modalTime: document.querySelector("#time"),
+  modalTask: document.querySelector("#task"),
 
-    openModal: function() {
-        this.openModalButton.addEventListener("click", () => {
-            this.modalDialog.showModal();
-            currentDateAndTime.run();
-        });
-    },
+  openModal: function () {
+    this.openModalButton.addEventListener("click", () => {
+      this.modalDialog.showModal();
+      currentDateAndTime.run();
+    });
+  },
 
-    closeModal: function() {
-        this.closeModalButton.addEventListener("click", () => {
+  closeModal: function () {
+    this.closeModalButton.addEventListener("click", () => {
+      if (
+        this.modalDate.value &&
+        this.modalTime.value &&
+        this.modalTask.value
+      ) {
+        createAndPlaceTaskInDom(
+          this.modalDate.value,
+          this.modalTime.value,
+          this.modalTask.value,
+        );
+        this.modalDialog.close();
+        this.modalTask.value = "";
+      }
+    });
+  },
 
-            if (this.modalDate.value && this.modalTime.value && this.modalTask.value) {
-                createAndPlaceTaskInDom(this.modalDate.value, this.modalTime.value, this.modalTask.value);
-                this.modalDialog.close();
-                this.modalTask.value = '';
-            }
-
-        });
-    },
-
-    run: function() {
-        this.openModal();
-        this.closeModal();
-    }
+  run: function () {
+    this.openModal();
+    this.closeModal();
+  },
 };
 
 modal.run();
 
 class Task {
-    constructor(date, time, task, id = null) {
-        this.id = id || Date.now().toString();
-        this.date = date;
-        this.time = time;
-        this.task = task;
-    }
+  constructor(date, time, task, id = null) {
+    this.id = id || Date.now().toString();
+    this.date = date;
+    this.time = time;
+    this.task = task;
+  }
 
-    createTaskDomElement() {
-        const div = document.createElement("div");
-        div.classList.add("task-item");
-        div.dataset.id = this.id;
+  createTaskDomElement() {
+    const div = document.createElement("div");
+    div.classList.add("task-item");
+    div.dataset.id = this.id;
 
-        div.innerHTML = `
+    div.innerHTML = `
             <div class="top"> 
                 <span class="x">${this.date}</span> 
                 <span class="y">${this.time}</span> 
@@ -92,66 +98,60 @@ class Task {
             <button class="delete-task"><img src="check.svg"></button>
             </div>
         `;
-        
-        div.querySelector('.delete-task').addEventListener('click', () => {
-            deleteTask(this.id);
-            div.remove();
-        });
-        
-        return div;
-    }
+
+    div.querySelector(".delete-task").addEventListener("click", () => {
+      deleteTask(this.id);
+      div.remove();
+    });
+
+    return div;
+  }
 }
 
 function createAndPlaceTaskInDom(date, time, task) {
-    const newTask = new Task(date, time, task);    
-    const taskElement = newTask.createTaskDomElement();
-    const main = document.querySelector("main");
+  const newTask = new Task(date, time, task);
+  const taskElement = newTask.createTaskDomElement();
+  const main = document.querySelector("main");
 
-    if (main) {
-        main.appendChild(taskElement);
-        saveTask(newTask);
-    }
+  if (main) {
+    main.appendChild(taskElement);
+    saveTask(newTask);
+  }
 }
 
 function saveTask(newTask) {
-    const tasks = getAllTasks(); 
-    tasks.push(newTask); 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+  const tasks = getAllTasks();
+  tasks.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function getAllTasks() {
-    const tasksJSON = localStorage.getItem('tasks');
-    return tasksJSON ? JSON.parse(tasksJSON) : [];
+  const tasksJSON = localStorage.getItem("tasks");
+  return tasksJSON ? JSON.parse(tasksJSON) : [];
 }
-
 
 function deleteTask(id) {
-    let tasks = getAllTasks();
-    tasks = tasks.filter(task => task.id !== id);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+  let tasks = getAllTasks();
+  tasks = tasks.filter((task) => task.id !== id);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
 
 function loadTasksFromLocalStorage() {
-    const tasks = getAllTasks();
-    const main = document.querySelector("main");
-    
-    if (main && tasks.length > 0) {
-        tasks.forEach(taskData => {
-            const task = new Task(
-                taskData.date, 
-                taskData.time, 
-                taskData.task, 
-                taskData.id
-            );
-            
-            main.appendChild(task.createTaskDomElement());
-        });
-    }
+  const tasks = getAllTasks();
+  const main = document.querySelector("main");
+
+  if (main && tasks.length > 0) {
+    tasks.forEach((taskData) => {
+      const task = new Task(
+        taskData.date,
+        taskData.time,
+        taskData.task,
+        taskData.id,
+      );
+
+      main.appendChild(task.createTaskDomElement());
+    });
+  }
 }
 
-
 document.addEventListener("DOMContentLoaded", loadTasksFromLocalStorage);
-
-
-
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  base: '/to-do/',
+  base: "/to-do/",
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
